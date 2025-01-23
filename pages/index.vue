2fae50f5fcf54e02e176d7e00fffd5216d789021
Sébastien JEAN <contact@sebjean.com>
2025-01-23 11:39:15 +0100

    ✨ Add links with setLocale

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
         <nuxt-link :to="localePath('/login')">{{ $t('views.login') }}</nuxt-link>
     </p>
 
+    <h2><pre>switchLocalePath</pre></h2>
+
     <ul>
         <li>
             <nuxt-link :to="switchLocalePath('fr')">Français</nuxt-link>
@@ -14,9 +16,22 @@
             <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
         </li>
     </ul>
+
+    <h2><pre>setLocale</pre></h2>
+
+    <ul>
+        <li>
+            <a href="#" @click.prevent="setLocale('fr')">Français</a>
+        </li>
+
+        <li>
+            <a href="#" @click.prevent="setLocale('en')">English</a>
+        </li>
+    </ul>
 </template>
 
 <script setup>
+const { setLocale } = useI18n()
 const localePath = useLocalePath()
 const switchLocalePath = useSwitchLocalePath()
 </script>
